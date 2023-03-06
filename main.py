5f59b37c841b95af2b23a572beeb8f1b6b5fdbad
sudamasharma <sudama.sharma@heptagon.in>
2023-03-06 19:13:06 +0530

    read generic file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 import pandas as pd
 import numpy as np
 from scipy import spatial
@@ -5,14 +7,23 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
 
-def get_embeddings():
-    df = pd.read_csv('lyrics.csv')
-    df = df.assign(embeddings=df['Lyric'].apply(lambda x: model.encode(str(x))))
-    print(df)
-    return df
 
-def closest_lyrics(inp):
-    data = get_embeddings()
+def get_embeddings(filename):
+    with open(filename) as csv_file:
+        # read the csv file
+        csv_reader = csv.reader(csv_file)
+
+    # now we can use this csv files into the pandas
+    df = pd.DataFrame([csv_reader], index=None)
+
+    df_embedding = df.assign(embeddings=df['Lyric'].apply(
+        lambda x: model.encode(str(x))))
+    print(df_embedding)
+    return df_embedding
+
+
+def closest_lyrics(inp, filename):
+    data = get_embeddings(filename)
     inp_vector = model.encode(inp)
     s = data['embeddings'].apply(
         lambda x: 1 - spatial.distance.cosine(x, inp_vector))
@@ -20,7 +31,8 @@ def closest_lyrics(inp):
     return (data.sort_values('similarity', ascending=False))
 
 
-
 if __name__ == '__main__':
 
-    print(closest_lyrics('thinking about you'))
+    filename = 'lyrics.csv'     # csv file name
+
+    print(closest_lyrics('thinking about you', filename))
