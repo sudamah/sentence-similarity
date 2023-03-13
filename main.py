fe2005aff0483a8aed5d8d8b16a60ca446455926
sudamasharma <sudama.sharma@heptagon.in>
2023-03-13 18:10:34 +0530

    add similarity score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import json
 
 import torch
 import numpy as np
@@ -10,26 +12,50 @@ from transformers import AutoTokenizer, AutoModel
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def load_data(filename):
-    # read the csv file
-    df = pd.read_csv(filename)
-    return df
+def load_data(filepath):
+    """
+    It takes a filepath as an argument, checks the file extension, and loads the file into a Pandas
+    DataFrame
+
+    :param filepath: The path to the file you want to load
+    :return: A dataframe
+    """
+    try:
+        # Checking if the filepath ends with .csv and if it does, it loads the file into a dataframe.
+        if filepath.endswith(".csv"):
+            df = pd.read_csv(filepath)
+
+        # Checking if the filepath ends with .xlsx and if it does, it loads the file into a dataframe.
+        elif filepath.endswith(".xlsx"):
+            df = pd.read_excel(filepath)
+
+        # Reading the text file and loading it into a dataframe.
+        elif filepath.endswith(".txt"):
+            df = pd.read_csv(filepath, sep=',', header=None)
+
+        # Loading the json file into a dataframe.
+        elif filepath.endswith(".json"):
+            with open(filepath, 'r') as file:
+                data = json.load(file)
+            df = pd.DataFrame(data)
+
+        else:
+            return None
+
+        return df
+
+    except:
+        return "Something went wrong"
 
 
-def sentence_list(df):
-    sentences = []
-    for key_str in tqdm(df.Occupation):
-        sentences.append(key_str)
+def similarity_score(sent_list, model, tokenizer):
+    """
+    It takes a list of sentences, tokenizes them, and then calculates the cosine similarity between each
+    sentence
 
-    return sentences
-
-
-def similarity_score(sent_list):
-
-    tokenizer = AutoTokenizer.from_pretrained(
-        'sentence-transformers/bert-base-nli-mean-tokens')
-    model = AutoModel.from_pretrained(
-        'sentence-transformers/bert-base-nli-mean-tokens')
+    :param sent_list: list of sentences
+    :return: A similarity matrix
+    """
 
     # initialize dictionary that will contain tokenized sentences
     tokens = {'input_ids': [], 'attention_mask': []}
@@ -64,8 +90,18 @@ def similarity_score(sent_list):
     return similarity_matrix
 
 
-def sentence_mapping(score, sentences):
-    rows = np.argwhere(score > .7)
+def sentence_mapping(score, sentences, threshold=0.7):
+    """
+    It takes the score matrix and the sentences and returns a dictionary with the key as the sentence
+    number and the value as a list of sentences that are similar to the key sentence
+
+    :param score: the similarity matrix
+    :param sentences: list of sentences
+    :return: A dictionary with the keys being the index of the sentence and the values being a list of
+    sentences that are similar to the key sentence.
+    """
+
+    rows = np.argwhere(score > threshold)
     temp_dict = {}
     for i in rows:
         if i[0] not in temp_dict.keys():
@@ -77,12 +113,27 @@ def sentence_mapping(score, sentences):
     return temp_dict
 
 
+# A way to tell the interpreter that the code in the block should only be executed if the file is run
+# as the main program.
 if __name__ == '__main__':
 
-    filename = 'information.csv'     # csv file name
+    # The path to the csv file that contains the sentences.
+    filename = '/home/heptagon/Desktop/sentence-similarity/information.csv'     # csv file name
+
+    # Loading the pretrained model.
+    tokenizer = AutoTokenizer.from_pretrained(
+        'sentence-transformers/bert-base-nli-mean-tokens')
+    model = AutoModel.from_pretrained(
+        'sentence-transformers/bert-base-nli-mean-tokens')
+
+    # Checking if the file is valid or not.
     data = load_data(filename)
+    if data is None:
+        print("pass the valid file")
 
-    sentences = sentence_list(data)
-    score = similarity_score(sentences)
+    # Taking the column 'Occupation' from the dataframe and converting it into a list.
+    sentences = data['Occupation'].to_list()
+    score = similarity_score(sentences, model, tokenizer)
 
+    # Printing the sentence mapping.
     print(sentence_mapping(score, sentences))
